d5f2b19c0194986b41ba3b5df2e13df080b5cd0c
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-07-31 17:01:36 +0000

    second commit

---
+++ StudyBuddy/base/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length= 200)
+    description = models.TextField(null= True, blank=True) # leave the field blank and update later
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
