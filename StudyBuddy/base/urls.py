6d733c4bd655ebfad2b3a3de6056665996fadd24
zezor <kemma2993@gmail.com>
2025-08-05 15:38:49 -0800

    Implement user profile functionality; add userProfile view, update URLs, and create profile template with activity and room information components

--- StudyBuddy/base/urls.py
+++ StudyBuddy/base/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     
     path('',views.home, name="home"),
     path('room/<int:pk>/', views.room, name="room"),
+    path('profile/<int:pk>/', views.userProfile, name="profile"),
 
     path('create_room/', views.createRoom, name="create_room"),
     path('update_room/<str:pk>/', views.updateRoom, name="update_room"),
--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -98,7 +98,8 @@ def home(request):
                                 )# filter from the database for selected objects
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q) | Q(room__name__icontains=q) | Q(room__description__icontains=q)).order_by('-created')[:5]  # Get the latest 5 messages related to the search query
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q) | Q(room__name__icontains=q) |
+                                           Q(room__description__icontains=q)).order_by('-created')[:5]  # Get the latest 5 messages related to the search query
 
     context ={'rooms': rooms, 'topics':topics, 'room_count': room_count, 'room_messages': room_messages}
     return render( request, 'base/home.html', context)
@@ -126,6 +127,14 @@ def room(request, pk):
     participants = room.participants.all()
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms':rooms, 'room_messages': room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
