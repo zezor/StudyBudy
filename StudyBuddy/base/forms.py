c4e4b7b55eb4511a8859d745994c356410f7b360
zezor <kemma2993@gmail.com>
2025-08-06 12:30:40 -0800

    Add main CSS styles and layout for StudyBuddy application
    - Created main.css for global styles including body background color.
    - Added style.css with comprehensive styles for various components, including buttons, avatars, dropdowns, headers, layouts, forms, and more.
    - Implemented responsive design adjustments for different screen sizes.
    - Established color variables for consistent theming across the application.

--- StudyBuddy/base/forms.py
+++ StudyBuddy/base/forms.py
@@ -6,6 +6,7 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants']
 
 class MessageForm(ModelForm):
     class Meta:
